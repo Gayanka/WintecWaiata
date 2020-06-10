3b2b0c3f8ec26a96f2a147e519fbd8c06cb3634d
AndrewChoi <kaho623@hotmail.com>
2020-06-11 01:23:26 +1200

    Migrate to MVVM in videoViewList

--- app/src/main/java/com/example/wintecwaiata/MainPageFragment.java
+++ app/src/main/java/com/example/wintecwaiata/MainPageFragment.java
@@ -1,19 +1,65 @@
 package com.example.wintecwaiata;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class MainPageFragment extends Fragment {
+    private VideoListViewModel videoListViewModel;
+    private RecyclerView recyclerView;
+    private TextView textView_songLength;
+    public static final String VIDEO_ID_NAME = "video_id";
+    public static final String VIDEO_TITLE_NAME = "video_title";
+    public static final String VIDEO_FILE_NAME = "video_file_name";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.mainpagefragment, container, false);
+        final View view =  inflater.inflate(R.layout.mainpagefragment, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerView_videolist);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        recyclerView.setHasFixedSize(true);
+
+        final VideoListAdapter videoListAdapter = new VideoListAdapter();
+        recyclerView.setAdapter(videoListAdapter);
+
+        textView_songLength = view.findViewById(R.id.textView_songLength);
+
+
+        videoListViewModel = new ViewModelProvider(this, new VideoListViewModelFactory(this.getActivity().getApplication()))
+                .get(VideoListViewModel.class);
+        videoListViewModel.getVideoListView().observe(this, new Observer<List<VideoListView>>() {
+            @Override
+            public void onChanged(final List<VideoListView> videoListViews) {
+                videoListAdapter.setVideoList(videoListViews);
+                textView_songLength.setText(videoListAdapter.getItemCount() + " songs");
+                videoListAdapter.setOnItemClickListener(new VideoListAdapter.OnItemClickListener() {
+                    @Override
+                    public void OnItemClick(int position) {
+                        Intent intent = new Intent(view.getContext(), VideoPlayerActivity.class);
+                        intent.putExtra(VIDEO_ID_NAME, videoListViews.get(position).getId());
+                        intent.putExtra(VIDEO_TITLE_NAME, videoListViews.get(position).getTitle());
+                        intent.putExtra(VIDEO_FILE_NAME, videoListViews.get(position).getFilename());
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+        return view;
     }
 }
